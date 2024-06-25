0fdb2a2fe81d8e4de9d8e3ced7f5acb472ce0801
OCKAPBIHKA <152022576+OCKAPBIHKA@users.noreply.github.com>
2024-06-25 21:45:25 +0300

    make project hw3

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <form>
+        <input type="text" name="login" />
+        <input type="password" name="password" />
+        <button type="submit">Log in</button>
+      </form>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,27 @@
+import { HiUser } from "react-icons/hi2";
+import { HiPhone } from "react-icons/hi2";
+import styles from "./Contact.module.css";
+
+function Contact({ contact, onDelete }) {
+  return (
+    <>
+      <div className={styles.profile}>
+        <div className={styles.user}>
+          <HiUser size={"18px"} title="contact icon" />
+          <span>{contact.name}</span>
+        </div>
+
+        <div className={styles.phone}>
+          <HiPhone size={"18px"} title="phone icon" />
+          <span>{contact.number}</span>
+        </div>
+      </div>
+
+      <button type="button" onClick={() => onDelete(contact.id)}>
+        Delete
+      </button>
+    </>
+  );
+}
+
+export default Contact;
